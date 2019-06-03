fb737f2debbc43e22d8cff06cdb5e104ec017518
Sebastian Fernandez Ledesma <sfernandezledesma@gmail.com>
2019-06-03 20:08:15 -0300

    SeAlimentaDe

--- data.sql
+++ data.sql
@@ -13,13 +13,14 @@ INSERT INTO Organismo(IdOrganismo, Nombre) VALUES (2, 'Organismo La Pampa');
 
 CREATE TABLE Provincia (
   IdProvincia INTEGER, 
-  Nombre VARCHAR(50), 
+  Nombre VARCHAR(50),
+  SuperficieTotal INTEGER,
   IdOrganismo INTEGER,
   PRIMARY KEY (IdProvincia),
   FOREIGN KEY (IdOrganismo) REFERENCES Organismo(IdOrganismo)
 );
-INSERT INTO Provincia(IdProvincia, Nombre, IdOrganismo) VALUES (1, 'Buenos Aires', 1);
-INSERT INTO Provincia(IdProvincia, Nombre, IdOrganismo) VALUES (2, 'La Pampa', 2);
+INSERT INTO Provincia(IdProvincia, Nombre, SuperficieTotal, IdOrganismo) VALUES (1, 'Buenos Aires', 307571, 1);
+INSERT INTO Provincia(IdProvincia, Nombre, SuperficieTotal, IdOrganismo) VALUES (2, 'La Pampa', 143440, 2);
 
 CREATE TABLE Parque (
   IdParque INTEGER, 
@@ -54,3 +55,73 @@ INSERT INTO Area(IdArea, Nombre, Superficie, IdParque) VALUES (1, '1.1', 2, 1);
 INSERT INTO Area(IdArea, Nombre, Superficie, IdParque) VALUES (2, '1.2', 2, 1);
 INSERT INTO Area(IdArea, Nombre, Superficie, IdParque) VALUES (3, '2.1', 2, 2);
 INSERT INTO Area(IdArea, Nombre, Superficie, IdParque) VALUES (4, '2.2', 2, 2);
+
+CREATE TABLE ElementoNatural (
+  IdElemento INTEGER, 
+  nombreCientifico VARCHAR(50), 
+  nombreVulgar VARCHAR(50), 
+  tipoElemento VARCHAR(50),
+  PRIMARY KEY (IdElemento),
+  CONSTRAINT elemento_tipos CHECK (tipoElemento = 'Mineral' OR tipoElemento = 'Comestible')
+);
+INSERT INTO ElementoNatural(IdElemento, nombreCientifico, nombreVulgar, tipoElemento) VALUES (1, 'Arena', 'Arena', 'Mineral');
+INSERT INTO ElementoNatural(IdElemento, nombreCientifico, nombreVulgar, tipoElemento) VALUES (2, 'Panthera tigris', 'Tigre', 'Comestible');
+INSERT INTO ElementoNatural(IdElemento, nombreCientifico, nombreVulgar, tipoElemento) VALUES (3, 'Oryctolagus cuniculus', 'Conejo', 'Comestible');
+INSERT INTO ElementoNatural(IdElemento, nombreCientifico, nombreVulgar, tipoElemento) VALUES (4, 'Trifolium', 'Trebol', 'Comestible');
+
+CREATE TABLE Mineral (
+  IdElemento INTEGER, 
+  tipoMineral VARCHAR(50),
+  PRIMARY KEY (IdElemento),
+  CONSTRAINT mineral_tipos CHECK (tipoMineral = 'Roca' OR tipoMineral = 'Cristal')
+);
+INSERT INTO Mineral(IdElemento, tipoMineral) VALUES (1, 'Roca');
+
+CREATE TABLE Comestible (
+  IdElemento INTEGER, 
+  tipoComestible VARCHAR(50),
+  PRIMARY KEY (IdElemento),
+  FOREIGN KEY (IdElemento) REFERENCES ElementoNatural(IdElemento),
+  CONSTRAINT comestible_tipos CHECK (tipoComestible = 'Vegetal' OR tipoComestible = 'Animal')
+);
+INSERT INTO Comestible(IdElemento, tipoComestible) VALUES (2, 'Animal');
+INSERT INTO Comestible(IdElemento, tipoComestible) VALUES (3, 'Animal');
+INSERT INTO Comestible(IdElemento, tipoComestible) VALUES (4, 'Vegetal');
+
+CREATE TABLE Vegetal (
+  IdElemento INTEGER, 
+  tipoVegetal VARCHAR(50),
+  PRIMARY KEY (IdElemento),
+  FOREIGN KEY (IdElemento) REFERENCES ElementoNatural(IdElemento),
+  CONSTRAINT vegetal_tipos CHECK (tipoVegetal = 'SinFloracion' OR tipoVegetal = 'ConFloracion')
+);
+INSERT INTO Vegetal (IdElemento, tipoVegetal) VALUES (4, 'ConFloracion');
+
+CREATE TABLE ConFloracion (
+  IdElemento INTEGER,
+  periodoFloracion VARCHAR(50),
+  PRIMARY KEY (IdElemento),
+  FOREIGN KEY (IdElemento) REFERENCES ElementoNatural(IdElemento)
+);
+INSERT INTO ConFloracion(IdElemento, periodoFloracion) VALUES (4, 'fines agosto hasta mediados marzo');
+
+CREATE TABLE Animal (
+  IdElemento INTEGER, 
+  tipoAlimentacion VARCHAR(50), 
+  periodoCelo VARCHAR(50),
+  PRIMARY KEY (IdElemento),
+  FOREIGN KEY (IdElemento) REFERENCES ElementoNatural(IdElemento),
+  CONSTRAINT tipo_alimentacion CHECK (tipoAlimentacion = 'Herbivoro' OR tipoAlimentacion = 'Carnivoro' OR tipoAlimentacion = 'Omnivoro')
+);
+INSERT INTO Animal(IdElemento, tipoAlimentacion, periodoCelo) VALUES (2, 'Carnivoro', 'Anual');
+INSERT INTO Animal(IdElemento, tipoAlimentacion, periodoCelo) VALUES (3, 'Herbivoro', 'Permanente');
+
+CREATE TABLE SeAlimentaDe (
+  IdDepredador INTEGER,
+  IdPresa INTEGER,
+  PRIMARY KEY (IdDepredador, IdPresa),
+  FOREIGN KEY (IdDepredador) REFERENCES Animal(IdElemento),
+  FOREIGN KEY (IdPresa) REFERENCES Comestible(IdElemento)
+);
+INSERT INTO SeAlimentaDe(IdDepredador, IdPresa) VALUES (2, 3);
+INSERT INTO SeAlimentaDe(IdDepredador, IdPresa) VALUES (3, 4);
